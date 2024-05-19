42f8ed680bca1b0566ddf7807d0bfd7f8fca8eee
Minami Ueda <minami.ueda@gmail.com>
2024-05-19 21:25:37 +0900

    Add fetchWithTimeout function

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,26 @@ export const decimalFloor = (value: number, base: number) => {
   return Math.floor(value * base) / base;
 }
 
+export const fetchWithTimeout = async (url: string, timeout: number = 3000, options?: RequestInit): Promise<Response> => {
+  try {
+    const response: Response = await Promise.race([
+      fetch(url, options),
+      new Promise((_, reject) =>
+        setTimeout(() => reject(new Error('Request timeout')), timeout)
+      ),
+    ]);
+
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
 export const fetchWithRetry = async (url: string, retries: number = 5, delay: number = 1000): Promise<Response> => {
   try {
     const response = await fetch(url);
